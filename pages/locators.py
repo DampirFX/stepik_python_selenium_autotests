5b41c67c5425326cf4792cc4168eff84d6c4c619
DampirFX <for_work_mail@list.ru>
2024-11-20 12:56:37 +0700

    move login methods to base_page

--- pages/locators.py
+++ pages/locators.py
@@ -15,5 +15,6 @@ class ProductPageLocators():
     MESSAGE_ABOUT_ADD_TO_BASKET = (By.CSS_SELECTOR,"#messages>div:nth-child(1)>.alertinner strong")
     MESSAGE_ABOUT_BASKET_TOTAL = (By.CSS_SELECTOR, "#messages>div:nth-child(3)>.alertinner strong")
 
-
-    # .alertinner p:nth-child(1)
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
